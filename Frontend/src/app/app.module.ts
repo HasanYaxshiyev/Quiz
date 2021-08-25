ec859ce2f820cd61ab8e0e6b3b5d3a4f558a7401
Hasanboy <xasanboy_2222@mail.ru>
2021-08-25 16:48:20 +0500

    added questioncomponent

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { QuestionComponent } from './question.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
