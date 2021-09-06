f020493058379e96efe9f08f98921c2188d261d2
Hasanboy <xasanboy_2222@mail.ru>
2021-09-06 17:01:15 +0500

    added angular material

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { QuestionComponent } from './question.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
+
   declarations: [
     AppComponent,
     QuestionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/app/question.component.ts
+++ Frontend/src/app/question.component.ts
@@ -1,9 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component({
-    template:'<h1>Bu savol</h1>',
+    template:'<h1>Bu savol</h1><button mat-button color="primary">test</button>',
     selector:'question'
 })
 export class QuestionComponent {
-
 }
